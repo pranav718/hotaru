fb49ffdf5c38726344341f87c59616704fb55fed
pranav718 <raypranav718@gmail.com>
2026-07-13 21:09:30 +0530

    update main to test rpc setup between two nodes

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/pranav718/hotaru/raft"
 )
@@ -9,13 +10,27 @@ import (
 func main() {
 	//creating 3 node cluster with node 0's peer being [1,2] and so on
 
-	node0 := raft.NewRaftNode(0, []int{1,2})
-	node1 := raft.NewRaftNode(1, []int{0,2})
-	node2 := raft.NewRaftNode(2, []int{0,1}) 
+	node0 := raft.NewRaftNode(0, []int{1,2}, ports)
+	node1 := raft.NewRaftNode(1, []int{0,2}, ports)
 
-	for i, node := range []*raft.RaftNode{node0, node1, node2} {
-		state, term := node.GetState()
-		fmt.Printf("[Test] node %d -> state: %s, term: %d\n", i, state, term)
+	err := node0.StartServer()
+	if err != nil {
+		fmt.Printf("error starting server 0: %v\n", err)
+		return
 	}
+	defer node0.StopServer()
 
+	err = node1.StartServer()
+	if err != nil {
+		fmt.Printf("error starting server 1: %v\n", err)
+		return
+	}
+	defer node1.StopServer()
+
+	time.Sleep(100 * time.Millisecond)
+
+	node0.TestTriggerSendRPC(1, false)
+	node0.TestTriggerSendRPC(1, true)
+
+	time.Sleep(500 * time.Millisecond)
 }
