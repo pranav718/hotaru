5ad2567aaecbf5ac42c29c5a89acf1d3622fa390
pranav718 <raypranav718@gmail.com>
2026-07-13 21:21:08 +0530

    update main to verify election timeouts

--- main.go
+++ main.go
@@ -8,29 +8,33 @@ import (
 )
 
 func main() {
-	//creating 3 node cluster with node 0's peer being [1,2] and so on
+	ports := map[int]string{
+		0: "127.0.0.1:8000",
+		1: "127.0.0.1:8001",
+		2: "127.0.0.1:8002",
+	}
 
-	node0 := raft.NewRaftNode(0, []int{1,2}, ports)
-	node1 := raft.NewRaftNode(1, []int{0,2}, ports)
+	node0 := raft.NewRaftNode(0, []int{1, 2}, ports)
+	node1 := raft.NewRaftNode(1, []int{0, 2}, ports)
+	node2 := raft.NewRaftNode(2, []int{0, 1}, ports)
 
-	err := node0.StartServer()
-	if err != nil {
+	if err := node0.StartServer(); err != nil {
 		fmt.Printf("error starting server 0: %v\n", err)
 		return
 	}
 	defer node0.StopServer()
 
-	err = node1.StartServer()
-	if err != nil {
+	if err := node1.StartServer(); err != nil {
 		fmt.Printf("error starting server 1: %v\n", err)
 		return
 	}
 	defer node1.StopServer()
 
-	time.Sleep(100 * time.Millisecond)
+	if err := node2.StartServer(); err != nil {
+		fmt.Printf("error starting server 2: %v\n", err)
+		return
+	}
+	defer node2.StopServer()
 
-	node0.TestTriggerSendRPC(1, false)
-	node0.TestTriggerSendRPC(1, true)
-
-	time.Sleep(500 * time.Millisecond)
+	time.Sleep(1 * time.Second)
 }
