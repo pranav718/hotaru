78fa5fa4fa201a1c016cf2df3f69344ff88f27a4
pranav718 <raypranav718@gmail.com>
2026-07-16 01:00:51 +0530

    added crash and recovery simulation tesetcase

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/pranav718/hotaru/raft"
@@ -14,23 +15,13 @@ func main() {
 		2: "127.0.0.1:8002",
 	}
 
+	for i := 0; i < 3; i++ {
+		os.Remove(fmt.Sprintf("raft_state_%d.json", i))
+	}
+
 	node0 := raft.NewRaftNode(0, []int{1, 2}, ports)
 	node1 := raft.NewRaftNode(1, []int{0, 2}, ports)
 	node2 := raft.NewRaftNode(2, []int{0, 1}, ports)
-
-	//set up divergent logs before starting servers
-	validLog := []raft.LogEntry{
-		{Term: 1, Index: 1, Command: "SET a = 1"},
-		{Term: 1, Index: 2, Command: "SET b = 2"},
-	}
-	node0.TestSetLogAndTerm(1, validLog)
-	node2.TestSetLogAndTerm(1, validLog)
-
-	divergentLog := []raft.LogEntry{
-		{Term: 3, Index: 1, Command: "SET a = 99"},
-		{Term: 3, Index: 2, Command: "SET b = 99"},
-	}
-	node1.TestSetLogAndTerm(1, divergentLog)
 
 	nodes := []*raft.RaftNode{node0, node1, node2}
 
@@ -53,12 +44,30 @@ func main() {
 		}
 	}
 
-	if leader != nil {
-		fmt.Println("[Test] Proposing new command to leader...")
-		leader.Propose("SET c = 3")
-	} else {
-		fmt.Println("[Test] No leader found in time.")
+	if leader == nil {
+		fmt.Println("[Test] No leader found.")
+		return
 	}
+
+	fmt.Println("[Test] Proposing first command...")
+	leader.Propose("SET x = 1")
+	time.Sleep(300 * time.Millisecond)
+
+	fmt.Println("[Test] Killing Node 2...")
+	node2.StopServer()
+	time.Sleep(100 * time.Millisecond)
+
+	fmt.Println("[Test] Proposing second command while Node 2 is offline...")
+	leader.Propose("SET y = 2")
+	time.Sleep(500 * time.Millisecond)
+
+	fmt.Println("[Test] Restarting Node 2...")
+	newNode2 := raft.NewRaftNode(2, []int{0, 1}, ports)
+	if err := newNode2.StartServer(); err != nil {
+		fmt.Printf("error restarting server 2: %v\n", err)
+		return
+	}
+	defer newNode2.StopServer()
 
 	time.Sleep(600 * time.Millisecond)
 }
