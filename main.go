b9da802b5f0022a4260c06a44cf72a4cf059b9f3
pranav718 <raypranav718@gmail.com>
2026-07-15 19:15:25 +0530

    added divergent log synchronization test case

--- main.go
+++ main.go
@@ -14,11 +14,25 @@ func main() {
 		2: "127.0.0.1:8002",
 	}
 
-	nodes := []*raft.RaftNode{
-		raft.NewRaftNode(0, []int{1, 2}, ports),
-		raft.NewRaftNode(1, []int{0, 2}, ports),
-		raft.NewRaftNode(2, []int{0, 1}, ports),
+	node0 := raft.NewRaftNode(0, []int{1, 2}, ports)
+	node1 := raft.NewRaftNode(1, []int{0, 2}, ports)
+	node2 := raft.NewRaftNode(2, []int{0, 1}, ports)
+
+	//set up divergent logs before starting servers
+	validLog := []raft.LogEntry{
+		{Term: 1, Index: 1, Command: "SET a = 1"},
+		{Term: 1, Index: 2, Command: "SET b = 2"},
 	}
+	node0.TestSetLogAndTerm(1, validLog)
+	node2.TestSetLogAndTerm(1, validLog)
+
+	divergentLog := []raft.LogEntry{
+		{Term: 3, Index: 1, Command: "SET a = 99"},
+		{Term: 3, Index: 2, Command: "SET b = 99"},
+	}
+	node1.TestSetLogAndTerm(1, divergentLog)
+
+	nodes := []*raft.RaftNode{node0, node1, node2}
 
 	for i, node := range nodes {
 		if err := node.StartServer(); err != nil {
@@ -28,7 +42,7 @@ func main() {
 		defer node.StopServer()
 	}
 
-	time.Sleep(500 * time.Millisecond)
+	time.Sleep(600 * time.Millisecond)
 
 	var leader *raft.RaftNode
 	for _, node := range nodes {
@@ -40,11 +54,11 @@ func main() {
 	}
 
 	if leader != nil {
-		fmt.Println("[Test] Leader found, proposing command...")
-		leader.Propose("SET x = 42")
+		fmt.Println("[Test] Proposing new command to leader...")
+		leader.Propose("SET c = 3")
 	} else {
 		fmt.Println("[Test] No leader found in time.")
 	}
 
-	time.Sleep(500 * time.Millisecond)
+	time.Sleep(600 * time.Millisecond)
 }
